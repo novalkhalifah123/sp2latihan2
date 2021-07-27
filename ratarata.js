d7879df10f458396c2784668125910af36b40332
novalkhalifah123 <87108201+novalkhalifah123@users.noreply.github.com>
2021-07-27 11:54:29 +0700

    Add files via upload

---
+++ ratarata.js
@@ -0,0 +1,8 @@
+var nilaiSantri = [5,9,6,7,9,8,10,7,8]
+var jumlahNilai = 0
+
+for(var n=0; n < nilaiSantri.length; n++){
+    jumlahNilai += nilaiSantri[n]
+}
+console.log(jumlahNilai)
+console.log(jumlahNilai/nilaiSantri.length)
