5ae3eb1ae01a40e15648ed89fd93d05e99f2536d
novalkhalifah123 <87108201+novalkhalifah123@users.noreply.github.com>
2021-07-27 13:44:45 +0700

    Add files via upload

---
+++ daftar.js
@@ -0,0 +1,27 @@
+var dataKelurahanBantul = [
+    ['Mulyodadi','Sidomulyo','Sumbermulyo'],
+    ['Banguntapan','Baturetno','Jagalan','Jambidan','Potorono','Singosaren','Tamanan','Wirokerten'],
+    ['Bantul','Palbapang','Ringinharjo','Sabdodadi','Trirenggo'],
+    ['Dlingo','Jatimulyo','Mangunan','Muntuk','TemuwuhTerong'],
+    ['Girirejo','Imogiri','Karangtalun','Karangtengah','Kebonagung','Selopamioro','Sriharjo','Wukirsari'],
+    ['Canden','Patalan','Sumberagung','Trimulyo'],
+    ['Bangunjiwo','Ngestiharjo','Tamantirto','Tirtonirmolo'],
+    ['Donotirto','Parangtritis','Tirtohargo','Tirtomulyo','Tirtosari'],
+    ['Guwosari','Sendangsari','Triwidadi'],
+    ['Caturharjo','Gilangharjo','TriharjoWijirejo'],
+    ['Srimulyo','Sitimulyo','Srimartani'],
+    ['Bawuran','Pleret','Segoroyoso','Wonokromo','Wonolelo'],
+    ['Panjangrejo','Bangunharjo','Srihardono'],
+    ['Gadingsari','Gadingharjo','Murtigading','Srigading'],
+    ['Argodadi','Argorejo','Argosari','Argomulyo'],
+    ['Bangunharjo','Panggungharjo','PendowoharjoT','imbulharjo'],
+    ['Poncosari','Trimurti'],
+
+
+
+]
+console.log(dataKelurahanBantul)
+
+
+
+    
