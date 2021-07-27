929abb8f9fa5d00ed9e598097581c786c06e487b
novalkhalifah123 <87108201+novalkhalifah123@users.noreply.github.com>
2021-07-27 14:28:26 +0700

    Add files via upload

---
+++ nomor2.js
@@ -0,0 +1,4 @@
+var reactNative = ['Hafif', 'Dayat', 'Wandi']
+var reactJs = ['Yusuf', 'Luthfi', 'Tompel']
+var JavaScriptDevision = reactNative.concat(reactJs);
+console.log("JavaScriptDevision : " + JavaScriptDevision)
